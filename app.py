abe97437030d9d0ce6471a59827e29a2202dc606
Ubuntu <ubuntu@ip-172-31-77-227.ec2.internal>
2025-08-28 15:22:07 +0000

    actualizacion zappa

---
+++ app.py
@@ -0,0 +1,5 @@
+import pandas as pd 
+from utils import is_prime
+def f(evebts, context):
+    print("hola desde lambda con zappa")
+    return{}
---
+++ utils.py
@@ -0,0 +1,6 @@
+def is_prime(n):
+    
+    for i in range(2,n):
+        if n % i == :
+            return False
+    return True
